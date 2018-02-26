8429b009ef99614bdf4ac7bdd46dc03838bb863d
Vladimir Kisilitsa <vkisilica@gmail.com>
2018-02-26 17:42:00 +0300

    Add two methods 'addPhone' and 'addEmail' to Client model

--- tests/Unit/Models/ClientTest.php
+++ tests/Unit/Models/ClientTest.php
@@ -6,6 +6,40 @@ use DigitalArts\Crm\SiteFormIntegration\Models\Client;
 
 class ClientTest extends Base
 {
+    /** @test */
+    public function add_phone()
+    {
+        $expects = [
+            'client_id' => 2,
+            'phone' => '987645321'
+        ];
+        $response = $this->createResponseMock(json_encode($expects));
+        $this->httpClient->method('request')->with('post', $this->getAddPhoneUri(), $expects)->willReturn($response);
+
+        $model = $this->getModel();
+        $clientModel = new $model($this->httpClient);
+        $actual = $clientModel->addPhone($expects['client_id'], $expects['phone']);
+
+        $this->assertEquals($expects, $actual);
+    }
+
+    /** @test */
+    public function add_email()
+    {
+        $expects = [
+            'client_id' => 2,
+            'email' => 'test@example.com'
+        ];
+        $response = $this->createResponseMock(json_encode($expects));
+        $this->httpClient->method('request')->with('post', $this->getAddEmailUri(), $expects)->willReturn($response);
+
+        $model = $this->getModel();
+        $clientModel = new $model($this->httpClient);
+        $actual = $clientModel->addEmail($expects['client_id'], $expects['email']);
+
+        $this->assertEquals($expects, $actual);
+    }
+
     protected function getModel()
     {
         return Client::class;
@@ -19,5 +53,15 @@ class ClientTest extends Base
     protected function getCreateUri()
     {
         return 'clients';
+    }
+
+    private function getAddPhoneUri()
+    {
+        return 'clients/phones';
+    }
+
+    private function getAddEmailUri()
+    {
+        return 'clients/emails';
     }
 }
