2fc7948522b17e3c9c287165ae61d1a4158ab39c
Vladimir Kisilitsa <vkisilica@gmail.com>
2018-04-11 17:53:07 +0300

    Add platform model

--- tests/Feature/SendFormToCrmTest.php
+++ tests/Feature/SendFormToCrmTest.php
@@ -5,6 +5,7 @@ namespace DigitalArts\Crm\SiteFormIntegration\Tests\Feature;
 use DigitalArts\Crm\SiteFormIntegration\Models\Client;
 use DigitalArts\Crm\SiteFormIntegration\Models\Lead;
 use DigitalArts\Crm\SiteFormIntegration\Models\LeadType;
+use DigitalArts\Crm\SiteFormIntegration\Models\Platform;
 use DigitalArts\Crm\SiteFormIntegration\Models\User;
 use DigitalArts\Crm\SiteFormIntegration\Tests\Base;
 use GuzzleHttp\Client as Guzzle;
@@ -44,6 +45,15 @@ class SendFormToCrmTest extends Base
                 ]
             ]
         ]);
+        $siteName = 'test.domain';
+        $streamPlatformContent = json_encode([
+            'data' => [
+                [
+                    "id" => 1,
+                    'name' => $siteName,
+                ]
+            ]
+        ]);
 
         $leadDescription = '';
         foreach ($data as $key => $value) {
@@ -69,6 +79,7 @@ class SendFormToCrmTest extends Base
         $expect = [
             'client_id' => 1,
             'lead_type_id' => 1,
+            'platform_id' => 1,
             'data' => [
                 'description' => $leadDescription
             ]
@@ -114,23 +125,31 @@ class SendFormToCrmTest extends Base
         $httpLeadType = $httpBuilder->getMock();
         $httpLeadType->method('request')->with('get', 'projects/7/lead-types', ['json' => ['name' => 'New Lead Type']])->willReturn($responseLeadType);
 
+        $responsePlatform = $this->createResponseMock($streamPlatformContent);
+        $httpPlatform = $httpBuilder->getMock();
+        $httpPlatform->method('request')->with('get', 'projects/7/platforms', ['json' => ['name' => $siteName]])->willReturn($responsePlatform);
+
         $responseLead = $this->createResponseMock($streamLeadContent);
         $httpLead = $httpBuilder->getMock();
         $httpLead->method('request')->with('post', 'leads', ['json' => $expect])->willReturn($responseLead);
 
         $clientModel = new Client($httpClient);
         $leadTypeModel = new LeadType($httpLeadType);
+        $platformModel = new Platform($httpPlatform);
         $leadModel = new Lead($httpLead);
 
         $clientModel->setProjectId(7);
         $leadTypeModel->setProjectId(7);
+        $platformModel->setProjectId(7);
 
         $clientInfo = $clientModel->where(['phone' => str_replace(['+', '(', ')', '-', ' '], '', $data['phone'])])->first();
         $leadTypeInfo = $leadTypeModel->where(['name' => $data['type']])->first();
+        $platformInfo = $platformModel->where(['name' => $siteName])->first();
 
         $lead = $leadModel->create([
             'client_id' => $clientInfo['id'],
             'lead_type_id' => $leadTypeInfo['id'],
+            'platform_id' => $platformInfo['id'],
             'data' => [
                 'description' => $leadDescription
             ]
